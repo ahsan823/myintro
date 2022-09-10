70433a65811057e0c8f3e60dd348a91c89828969
Muhammad Ahsan <108583492+ahsan823@users.noreply.github.com>
2022-09-10 12:12:03 +0500

    first
    first

---
+++ index.js
@@ -0,0 +1,24 @@
+var options = {
+  strings: ["Web Developer!", "Coder.", "Teacher.", "Programmer."],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+};
+
+var typed = new Typed(".auto-text", options);
+
+// let navbar = document.getElementById("navbar");
+// console.log(navbar);
+// setInterval(() => {
+//   let a = scrollY;
+//   if (a > 40) {
+//     navbar.style.background = "#fff";
+//     navbar.style.position = "fixed";
+//     navbar.style.boxShadow = "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px";
+//   } else {
+//     navbar.style.position = "static";
+//     navbar.style.background = "transparent";
+//     navbar.style.boxShadow = "none";
+//   }
+//   console.log(a);
+// }, 500);
